373a0802d1596abb4c82d7caedc0adac06c5b509
Alexandre jublot <alexjublot@gmail.com>
2022-10-20 20:26:13 +0200

    feat: added DtoHandler struct

---
+++ include/Polymorph/Network/DtoHandler.hpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2020
+** DtoHandler.hpp
+** File description:
+** header for DtoHandler.c
+*/
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include <functional>
+#include "Polymorph/Network/PacketHeader.hpp"
+#include "Polymorph/Network/SerializerTrait.hpp"
+
+namespace Polymorph::Network
+{
+
+    template<typename T>
+    struct DtoHandler
+    {
+        private:
+            static std::map<int, std::vector<std::function<void(PacketHeader, T &)>>> _handlers;
+
+        public:
+            static void registerHandler(int id, std::function<void(PacketHeader, T &)> handler)
+            {
+                _handlers[id].push_back(handler);
+            }
+
+            static void handleReceivedPacket(PacketHeader packetHeader, std::vector<std::byte> &bytes)
+            {
+                T payload = SerializerTrait<T>::deserialize(bytes);
+                for (auto &handler: _handlers[packetHeader.opId])
+                    handler(packetHeader, payload);
+            }
+
+            static void unregisterHandler(int opId)
+            {
+                _handlers.erase(opId);
+            }
+    };
+}
