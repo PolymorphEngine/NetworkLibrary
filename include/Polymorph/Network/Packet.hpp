b42ec9481c841e82d43a56ed61ebf6618cba18c8
Alexandre jublot <alexjublot@gmail.com>
2022-10-20 20:25:21 +0200

    feat: added Packet struct

---
+++ include/Polymorph/Network/Packet.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2020
+** Packet.hpp
+** File description:
+** header for Packet.c
+*/
+
+#pragma once
+
+#include "Polymorph/Network/PacketHeader.hpp"
+
+namespace Polymorph::Network
+{
+
+#ifdef _WIN32
+#pragma pack(push, 1)
+#endif
+    /**
+     * @brief Packet struct to store header and payload
+     * @tparam T
+     */
+    template<typename T>
+    struct Packet
+    {
+        PacketHeader header;
+        T payload;
+    };
+#ifdef _WIN32
+    ;
+    #pragma pack(pop)
+#else
+    __attribute__((packed));
+#endif
+
+}
