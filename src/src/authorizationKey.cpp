ef5149223c1800e7ca440eb12d054b06792ecc32
Alexandre jublot <alexjublot@gmail.com>
2022-10-21 19:58:05 +0200

    feat: added authorizationkey namespace and related functions

---
+++ src/src/authorizationKey.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2022
+** authorizationKey.cpp
+** File description:
+** authorizationKey.cpp
+*/
+
+
+#include <cstring>
+#include <random>
+#include "authorizationKey.hpp"
+
+namespace polymorph::network::authorizationKey
+{
+    AuthorizationKey generate()
+    {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        static std::uniform_int_distribution<> dis(0, 255);
+        static AuthorizationKey zero{0};
+        AuthorizationKey key{0};
+
+        while (areSame(key, zero)) {
+            for (auto &byte: key)
+                byte = dis(gen);
+        }
+        return key;
+    }
+
+    bool areSame(const AuthorizationKey &key, const AuthorizationKey &other)
+    {
+        return std::memcmp(key.data(), other.data(), key.size()) == 0;
+    }
+}
