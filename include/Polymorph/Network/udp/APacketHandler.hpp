a750684918e1839c3df031d36a85bce02647eb99
Alexandre jublot <alexjublot@gmail.com>
2022-10-24 12:14:31 +0200

    feat: added APacketHandler

---
+++ include/Polymorph/Network/udp/APacketHandler.hpp
@@ -0,0 +1,146 @@
+/*
+** EPITECH PROJECT, 2020
+** APacketHandler.hpp
+** File description:
+** header for APacketHandler.c
+*/
+
+
+#pragma once
+
+#include <map>
+#include <functional>
+#include <asio/ip/udp.hpp>
+#include <thread>
+#include "Polymorph/Network/udp/IPacketHandler.hpp"
+#include "Polymorph/Network/Packet.hpp"
+#include "Polymorph/Network/SerializerTrait.hpp"
+
+namespace polymorph::network::udp
+{
+    class Connector;
+
+    class APacketHandler : public IPacketHandler
+    {
+
+////////////////////// CONSTRUCTORS/DESTRUCTORS /////////////////////////
+
+        public:
+            APacketHandler(std::map<OpId, std::vector<std::function<void(const PacketHeader &, const std::vector<std::byte> &)>>> &callbacks, asio::ip::udp::endpoint endpoint);
+
+            ~APacketHandler();
+
+
+//////////////////////--------------------------/////////////////////////
+
+
+
+///////////////////////////// PROPERTIES ////////////////////////////////
+        protected:
+            /**
+             * @property The asio context used to create the socket
+             */
+            asio::io_context _context;
+
+            /**
+             * @property The socket to use to send and receive packets
+             */
+            asio::ip::udp::socket _socket;
+
+            /**
+             * @property The link to a Connector instance to send packets
+             */
+            std::shared_ptr<Connector> _connector;
+
+        private:
+            /**
+             * @property The thread used to run the io_context
+             */
+            std::thread _thread;
+
+            /**
+             * @property The callbacks to call when a packet is received
+             */
+            std::map<OpId, std::vector<std::function<void(const PacketHeader &, const std::vector<std::byte> &)>>> _receiveCallbacks;
+
+            /**
+             * @property The callbacks to call when a packet is sent
+             */
+            std::map<std::pair<asio::ip::udp::endpoint, PacketId>, std::function<void(const PacketHeader &, const std::vector<std::byte> &)>> _sentCallbacks;
+
+
+
+//////////////////////--------------------------/////////////////////////
+
+
+
+/////////////////////////////// METHODS /////////////////////////////////
+        public:
+            /**
+             * @copydetails IPacketHandler::packetReceived
+             */
+            void packetReceived(asio::ip::udp::endpoint from, const std::vector<std::byte> &bytes) override final;
+
+            /**
+             * @copydetails IPacketHandler::getPreparedSocket
+             */
+            asio::ip::udp::socket &getPreparedSocket() override final;
+
+            /**
+             * @copydetails IPacketHandler::start
+             */
+            void start() override final;
+
+            /**
+             * @brief Add a callback to call when a packet is received with the passed opId
+             * @tparam T The type of the packet to handle
+             * @param opId The opId of the packet to handle
+             * @param callback The callback to call when a packet is received
+             */
+            template<typename T>
+            void registerReceiveHandler(OpId opId, std::function<void(const PacketHeader &, const T &)> callback)
+            {
+                _receiveCallbacks[opId].push_back([callback](const PacketHeader &header, const std::vector<std::byte> &bytes) {
+                    Packet<T> packet = SerializerTrait<Packet<T>>::deserialize(bytes);
+                    callback(header, packet.payload);
+                });
+            }
+
+            /**
+             * @brief Remove all callbacks registered for the passed opId
+             * @param opId The opId of the packet to remove the callbacks from
+             */
+            void unregisterReceiveHandlers(OpId opId);
+
+            /**
+             * @brief Set the _connector property
+             * @param connector The connector to link to
+             */
+            void setConnector(std::shared_ptr<Connector> connector);
+
+        protected:
+            /**
+             * @brief add a callback to call when a packet with a specific packet id is sent
+             */
+            void _addSendCallback(const asio::ip::udp::endpoint& to, PacketId id, std::function<void(const PacketHeader &, const std::vector<std::byte> &)> callback);
+
+            /**
+             * @brief call a callback registered for the packet id and remove it
+             */
+            void _callAndPopSendCallback(asio::ip::udp::endpoint to, const PacketHeader &header, const std::vector<std::byte> &bytes);
+
+        private:
+            /**
+             * @brief call all registered receive callbacks for the packet
+             * @param header The header of the packet
+             * @param bytes The packet in its serialized form
+             */
+            void _broadcastReceivedPacket(const PacketHeader &header, const std::vector<std::byte> &bytes);
+
+
+
+//////////////////////--------------------------/////////////////////////
+
+    };
+
+}
---
+++ src/src/udp/APacketHandler.cpp
@@ -0,0 +1,94 @@
+/*
+** EPITECH PROJECT, 2022
+** APacketHandler.cpp
+** File description:
+** APacketHandler.cpp
+*/
+
+
+#include <asio/ip/udp.hpp>
+#include <iostream>
+#include "Polymorph/Network/udp/APacketHandler.hpp"
+
+polymorph::network::udp::APacketHandler::APacketHandler(
+        std::map<OpId, std::vector<std::function<void(const PacketHeader &, const std::vector<std::byte> &)>>> &callbacks,
+        asio::ip::udp::endpoint endpoint) : _context(), _socket(_context, endpoint), _receiveCallbacks(std::move(callbacks))
+{
+    if (_receiveCallbacks.contains(0)) {
+        std::cerr << "OpId 0 is reserved for internal use" << std::endl;
+        _receiveCallbacks.erase(0);
+    }
+    if (_receiveCallbacks.contains(1)) {
+        std::cerr << "OpId 1 is reserved for internal use" << std::endl;
+        _receiveCallbacks.erase(1);
+    }
+}
+
+polymorph::network::udp::APacketHandler::~APacketHandler()
+{
+    _context.stop();
+    _thread.join();
+}
+
+void polymorph::network::udp::APacketHandler::packetReceived(asio::ip::udp::endpoint from, const std::vector<std::byte> &bytes)
+{
+    PacketHeader header{0};
+
+    try {
+        header = SerializerTrait<PacketHeader>::deserialize(bytes);
+    } catch (const exceptions::DeserializingException &e) {
+        std::cerr << "Error while deserializing packet header: " << e.what() << std::endl;
+        return;
+    }
+    _broadcastReceivedPacket(header, bytes);
+}
+
+asio::ip::udp::socket &polymorph::network::udp::APacketHandler::getPreparedSocket()
+{
+    return _socket;
+}
+
+void polymorph::network::udp::APacketHandler::start()
+{
+    _thread = std::thread([this]() {
+        _context.run();
+    });
+}
+
+void polymorph::network::udp::APacketHandler::unregisterReceiveHandlers(polymorph::network::OpId opId)
+{
+    _receiveCallbacks.erase(opId);
+}
+
+void polymorph::network::udp::APacketHandler::setConnector(std::shared_ptr<Connector> connector)
+{
+    _connector = std::move(connector);
+}
+
+void
+polymorph::network::udp::APacketHandler::_addSendCallback(const asio::ip::udp::endpoint& to, polymorph::network::PacketId id,
+        std::function<void(const PacketHeader &, const std::vector<std::byte> &)> callback)
+{
+    _sentCallbacks[std::make_pair(to, id)] = std::move(callback);
+}
+
+void polymorph::network::udp::APacketHandler::_callAndPopSendCallback(asio::ip::udp::endpoint to,
+        const polymorph::network::PacketHeader &header, const std::vector<std::byte> &bytes)
+{
+    auto it = _sentCallbacks.find(std::make_pair(to, header.pId));
+
+    if (it != _sentCallbacks.end()) {
+        it->second(header, bytes);
+        _sentCallbacks.erase(it);
+    }
+}
+
+void polymorph::network::udp::APacketHandler::_broadcastReceivedPacket(const polymorph::network::PacketHeader &header,
+        const std::vector<std::byte> &bytes)
+{
+    if (_receiveCallbacks.contains(header.opId)) {
+        for (auto &callback : _receiveCallbacks[header.opId]) {
+            callback(header, bytes);
+        }
+    }
+}
