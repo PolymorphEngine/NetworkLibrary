667f958f7e74df5fea5be89f4ff0fdc410639dba
Alexandre jublot <alexjublot@gmail.com>
2022-10-25 21:47:29 +0200

    feat: added ClientConnection partial implementation

---
+++ src/include/tcp/ClientConnection.hpp
@@ -0,0 +1,84 @@
+/*
+** EPITECH PROJECT, 2020
+** ClientConnection.hpp
+** File description:
+** header for ClientConnection.c
+*/
+
+
+#pragma once
+
+#include <memory>
+#include <asio/ip/udp.hpp>
+#include <queue>
+#include "Polymorph/Network/SessionStore.hpp"
+#include "Polymorph/Network/PacketHeader.hpp"
+#include "Polymorph/Network/tcp/IConnectionPool.hpp"
+#include "Polymorph/Network/tcp/IPacketHandler.hpp"
+
+namespace polymorph::network::tcp
+{
+
+    class ClientConnection : public std::enable_shared_from_this<ClientConnection>
+    {
+
+////////////////////// CONSTRUCTORS/DESTRUCTORS /////////////////////////
+
+        public:
+            ClientConnection(asio::ip::tcp::socket socket, SessionStore &sessionStore, IConnectionPool &pool, IPacketHandler &packetHandler);
+
+            ~ClientConnection() = default;
+
+
+//////////////////////--------------------------/////////////////////////
+
+
+
+///////////////////////////// PROPERTIES ////////////////////////////////
+        public:
+
+
+        private:
+            SessionStore &_sessionAttributor;
+            IConnectionPool &_connectionPool;
+            IPacketHandler &_packetHandler;
+
+            std::atomic<bool> _stopped;
+
+            std::queue<std::pair<std::vector<std::byte>, std::function<void (const PacketHeader &, const std::vector<std::byte> &)>>> _sendQueue;
+            std::atomic<bool> _writeInProgress;
+            std::mutex _sendQueueMutex;
+
+            std::vector<std::byte> _receiveBuffer;
+            std::array<std::byte, 1024> _internalBuffer;
+
+            asio::ip::tcp::socket _socket;
+            SessionId _sessionId;
+
+
+//////////////////////--------------------------/////////////////////////
+
+
+
+/////////////////////////////// METHODS /////////////////////////////////
+        public:
+            void start();
+
+            void send(std::vector<std::byte> data, std::function<void (const PacketHeader &, const std::vector<std::byte> &)> callback = nullptr);
+
+        private:
+            void _doSend();
+
+            void _doReceive();
+
+            void _handleHandshakePacket(const PacketHeader &header, const std::vector<std::byte> &bytes);
+
+            bool _broadcastReceivedPacket(const PacketHeader &header, const std::vector<std::byte> &bytes);
+
+            bool _determinePacket(const PacketHeader &header, const std::vector<std::byte> &bytes);
+
+
+//////////////////////--------------------------/////////////////////////
+
+    };
+}
---
+++ src/src/tcp/ClientConnection.cpp
@@ -0,0 +1,132 @@
+/*
+** EPITECH PROJECT, 2022
+** ClientConnection.cpp
+** File description:
+** ClientConnection.cpp
+*/
+
+
+#include <iostream>
+#include "tcp/ClientConnection.hpp"
+#include "Polymorph/Network/SerializerTrait.hpp"
+#include "Polymorph/Network/dto/ConnectionDto.hpp"
+#include "authorizationKey.hpp"
+#include "Polymorph/Network/dto/ConnectionResponseDto.hpp"
+#include "Polymorph/Network/exceptions/UnauthorizedException.hpp"
+
+
+polymorph::network::tcp::ClientConnection::ClientConnection(asio::ip::tcp::socket socket, SessionStore &sessionStore, IConnectionPool &pool, IPacketHandler &packetHandler)
+        : _sessionAttributor(sessionStore), _connectionPool(pool), _packetHandler(packetHandler), _stopped(false), _writeInProgress(false), _socket(std::move(socket))
+{}
+
+void polymorph::network::tcp::ClientConnection::start()
+{
+    _connectionPool.join(shared_from_this());
+    _doReceive();
+}
+
+void polymorph::network::tcp::ClientConnection::send(std::vector <std::byte> data, std::function<void(const PacketHeader &, const std::vector <std::byte> &)> callback)
+{
+    std::unique_lock<std::mutex> lock(_sendQueueMutex);
+
+    _sendQueue.emplace(std::move(data), std::move(callback));
+    if (!_writeInProgress.exchange(true)) {
+        lock.unlock();
+        _doSend();
+    }
+}
+
+void polymorph::network::tcp::ClientConnection::_doSend()
+{
+    std::lock_guard<std::mutex> lock(_sendQueueMutex);
+    auto p = _sendQueue.front();
+    auto self(shared_from_this());
+    _socket.async_send(asio::buffer(p.first), [this, self](const asio::error_code &error, std::size_t /* bytesSent */) mutable {
+        if (error || _stopped) {
+            _stopped = true;
+            _connectionPool.leave(shared_from_this());
+            return;
+        }
+        std::unique_lock<std::mutex> lock(_sendQueueMutex);
+        auto p = _sendQueue.front();
+        auto header = SerializerTrait<PacketHeader>::deserialize(p.first);
+        if (p.second)
+            p.second(header, p.first);
+        _sendQueue.pop();
+        if (_sendQueue.empty()) {
+            _writeInProgress = false;
+            return;
+        }
+        lock.unlock();
+        _doSend();
+    });
+}
+
+void polymorph::network::tcp::ClientConnection::_doReceive()
+{
+    auto self(shared_from_this());
+    _socket.async_receive(asio::buffer(_internalBuffer), [this, self](const asio::error_code &error, std::size_t bytesReceived) {
+        if (error || _stopped) {
+            _stopped = true;
+            _connectionPool.leave(shared_from_this());
+            return;
+        }
+        _receiveBuffer.insert(_receiveBuffer.end(), _internalBuffer.begin(), _internalBuffer.begin() + bytesReceived);
+        while (_receiveBuffer.size() > sizeof(PacketHeader) ) {
+            auto header = SerializerTrait<PacketHeader>::deserialize(_receiveBuffer);
+            if (_receiveBuffer.size() >= sizeof(PacketHeader) + header.pSize) {
+                if (!_determinePacket(header, std::vector<std::byte>(_receiveBuffer.begin() + sizeof(PacketHeader), _receiveBuffer.begin() + sizeof(PacketHeader) + header.pSize)))
+                    return;
+                _receiveBuffer.erase(_receiveBuffer.begin(), _receiveBuffer.begin() + sizeof(PacketHeader) + header.pSize);
+            } else
+                break;
+        }
+        _doReceive();
+    });
+}
+
+void polymorph::network::tcp::ClientConnection::_handleHandshakePacket(const PacketHeader &header,
+                                                                       const std::vector <std::byte> &bytes)
+{
+    auto packet = SerializerTrait<Packet<ConnectionDto>>::deserialize(bytes);
+
+    if (authorizationKey::areSame(packet.payload.authKey, authorizationKey::zero)) {
+        _sessionId = _sessionAttributor.registerClient(_socket.remote_endpoint());
+        ConnectionResponseDto response{ .authorized = true};
+        // TODO change _packetHandler type to Server and call sendTo
+        return;
+    }
+
+    try {
+        _sessionAttributor.registerAuthoredClient(_socket.remote_endpoint(), packet.payload.sessionId, packet.payload.authKey);
+        ConnectionResponseDto response{ .authorized = true};
+        // TODO change _packetHandler type to Server and call sendTo
+    } catch(const exceptions::UnauthorizedException &e) {
+        std::cerr << "Error while registering client: " << e.what() << std::endl;
+        _sessionId = _sessionAttributor.registerClient(_socket.remote_endpoint());
+        ConnectionResponseDto response{ .authorized = false};
+        // TODO change _packetHandler type to Server and call sendTo
+    }
+}
+
+bool polymorph::network::tcp::ClientConnection::_broadcastReceivedPacket(const PacketHeader &header,
+                                                                         const std::vector <std::byte> &bytes)
+{
+    if (!_packetHandler.packetReceived(header, bytes)) {
+        _stopped = true;
+        _connectionPool.leave(shared_from_this());
+        return false;
+    };
+    return true;
+}
+
+bool polymorph::network::tcp::ClientConnection::_determinePacket(const polymorph::network::PacketHeader &header,
+                                                                 const std::vector<std::byte> &bytes)
+{
+    if (header.opId == ConnectionDto::opId) {
+        _handleHandshakePacket(header, bytes);
+        return true;
+    } else {
+        return _broadcastReceivedPacket(header, bytes);
+    }
+}
