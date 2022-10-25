3c27f9ef5924e483d01e3e6dd18213625c181056
EnergyCube <32596278+EnergyCube@users.noreply.github.com>
2022-10-25 19:23:23 +0200

    feat: IPacketHandler added

---
+++ include/Polymorph/Network/tcp/IPacketHandler.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2020
+** IPacketHandler.hpp
+** File description:
+** header for IPacketHandler.c
+*/
+
+#pragma once
+
+#include "Polymorph/Network/PacketHeader.hpp"
+#include <vector>
+
+namespace polymorph::network::tcp {
+
+    class IPacketHandler {
+
+        public:
+            virtual bool packetReceived(const PacketHeader &, const std::vector<std::byte> &bytes) = 0;
+
+    };
+}
