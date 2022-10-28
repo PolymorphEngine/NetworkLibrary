97ddbc8fad34fe19042861be151fc0ed3b4d8cb0
Alexandre jublot <alexjublot@gmail.com>
2022-10-28 19:42:28 +0200

    merge: hotfix/clientErrorMessage into dev

--- src/src/tcp/Client.cpp
+++ src/src/tcp/Client.cpp
@@ -28,6 +28,8 @@ void polymorph::network::tcp::Client::connect(std::function<void(bool, SessionId
             return;
         }
 
+        _doReceive();
+
         ConnectionDto dto{ 0, 0};
         send<ConnectionDto>(ConnectionDto::opId, dto);
         _isConnecting = false;
@@ -38,7 +40,6 @@ void polymorph::network::tcp::Client::connect(std::function<void(bool, SessionId
             return true;
         });
     });
-    _doReceive();
     _run();
 }
 
@@ -46,11 +47,17 @@ void polymorph::network::tcp::Client::connectWithSession(polymorph::network::Ses
                                                          polymorph::network::AuthorizationKey authKey, std::function<void(bool, SessionId)> callback)
 {
     _socket.async_connect(_serverEndpoint, [this, callback, session, authKey](std::error_code ec) {
+        if (ec == asio::error::operation_aborted) {
+            return;
+        }
         if (ec) {
             std::cerr << "Error while connecting to server: " << ec.message() << std::endl;
             callback(false, 0);
             return;
         }
+
+        _doReceive();
+
         ConnectionDto dto{ .sessionId = session, .authKey = authKey };
         send<ConnectionDto>(ConnectionDto::opId, dto);
         _isConnecting = false;
@@ -61,7 +68,6 @@ void polymorph::network::tcp::Client::connectWithSession(polymorph::network::Ses
             return true;
         });
     });
-    _doReceive();
     _run();
 }
 
@@ -92,7 +98,7 @@ void polymorph::network::tcp::Client::_doSend()
 void polymorph::network::tcp::Client::_doReceive()
 {
     _socket.async_receive(asio::buffer(_internalBuffer), [this](const asio::error_code &error, std::size_t bytesReceived) {
-        if (error == asio::error::operation_aborted)
+        if (error == asio::error::operation_aborted || error == asio::error::eof)
             return;
         if (error) {
             std::cerr << "Error while receiving packet: " << error.message() << std::endl;
